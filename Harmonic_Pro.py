75f4e4cbfb095107551e048fec19dd1c21f77ba3
piyushsrii <piyushnamo888@gmail.com>
2020-09-24 17:21:48 +0530

    [Piyush] Added : Harmonic value

--- Harmonic_Pro.py
+++ Harmonic_Pro.py
@@ -1,5 +1,5 @@
 n= int(input("enter the value of n"))  #initialize the value
 s=0.0
-for i in range(1, n + 1):   #Using for loop to determine the value of harmonically
+for i in range(1, n + 1):   #Using for loop to determine the value of git
         s = s + 1 / i ;     #store the value of each iteration
 print("Sum is", round(s,3))  #using round function to give out put by round off
